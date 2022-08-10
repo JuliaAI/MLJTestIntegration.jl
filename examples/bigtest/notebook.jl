4c6aa85b48f46e85fa2d7cdf738fb08207a54a7f
Anthony D. Blaom <anthony.blaom@gmail.com>
2022-08-10 16:53:48 +1200

    update readme and bigtest example

--- examples/bigtest/notebook.jl
+++ examples/bigtest/notebook.jl
@@ -67,9 +67,6 @@ known_problems = models() do model
         (name = "DecisionTreeClassifier", package_name="BetaML"),
         (name="RandomForestClassifier", package_name="BetaML"),
 
-        #https://github.com/JuliaAI/MLJLinearModels.jl/issues/123
-        (name= "MultinomialClassifier", package_name="MLJLinearModels"),
-
         # https://github.com/alan-turing-institute/MLJ.jl/issues/939
         (name = "NuSVC", package_name="LIBSVM"),
     ]
